27f270ae402aed1f14935c2a5ce2f7fd618d8278
Asep Solahudin <solahudinasep12@gmail.com>
2025-05-28 07:23:45 +0700

    Project Web Dakwah

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password123'),
+            'role_id' => 1,
+            'active_status' => 1,
+            'avatar' => 'avatar.png',
+            'dark_mode' => 0,
+        ]);
+        //
+    }
+}
